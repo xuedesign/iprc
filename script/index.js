81910eea0121e5275b2ccdcc1293656fc29e2bac
xuedesign <elmo@xuedesign.tw>
2019-06-02 20:26:44 +0800

    網頁優化 #20190602 - 新增GTM追蹤碼 - 新增WOW載入動畫

--- script/index.js
+++ script/index.js
@@ -10,16 +10,16 @@ loadjs('https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js', function() {
 //頂端浮動選單
 $(function(){$(window).load(function(){$(window).bind("scroll resize",function(){var o=$(this),t=o.scrollTop();180>t&&$("#top-bar").stop().animate({top:"-65px"}),t>180&&$("#top-bar").stop().animate({top:"0px"})}).scroll()})});
 
+//wow載入動畫
+loadjs('script/lib/wow.min.js', function() {
+  new WOW().init();
+});
+
 //返回頂端
 loadjs(['script/lib/jquery.ui.totop.min.js', 'https://cdn.jsdelivr.net/npm/jquery.easing@1.4.1/jquery.easing.min.js'], function() {
     $(document).ready(function() {
 	$().UItoTop({ easingType: 'easeOutQuart' });
     });
-});
-
-//wow載入動畫
-loadjs('script/lib/wow.min.js', function() {
-  new WOW().init();
 });
 
 //行動版選單
